676fdad5f4987cf622760d31680403e51c02abe5
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2017-06-08 17:05:25 +0200

    ENH: major cleanup + pep8 + docstrings

--- pyripser.py
+++ pyripser.py
@@ -1,70 +1,102 @@
+"""Pyrpser is a Python wrapper to the executable ripser:
+  https://github.com/Ripser/ripser
+
+With this wrapper, it is possible to compute the births and deaths of
+homology classes of Vietoris-Rips complexes up to the desired order(s)
+for a given (Numpy) distance matrix.
+
+Copyright Emanuele Olivetti and Stefania Ebli, 2017
+
+MIT License.
+
+"""
+
 import numpy as np
 import subprocess
 from tempfile import mktemp
 from os import remove
 
-
-def ripser(dm, ord_max=2, ripser_pathname='/home/stefania/ripser/ripser', ripser_format='distance'):
-	matrix_filename = mktemp()
-	np.savetxt(matrix_filename, dm, delimiter=' ')
-	result = execute_and_parse(matrix_filename=matrix_filename, ord_max=ord_max, ripser_pathname=ripser_pathname, ripser_format=ripser_format)
-	remove(matrix_filename)
-	return result
+RIPSER_PATHNAME = '/home/ele/local/src/ripser/ripser'
 
 
-def execute_and_parse(matrix_filename, ord_max=2, ripser_pathname='/home/stefania/ripser/ripser', ripser_format='distance'):
-	proc = subprocess.Popen([ripser_pathname, '--dim', str(ord_max), '--format', ripser_format, matrix_filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	output =''
-	for line in proc.stdout:
-		print(line[:-1])
-		output += line
+def ripser(dm, ord_max=2, ripser_pathname=RIPSER_PATHNAME,
+           ripser_format='distance'):
+    """Higher-level wrapper of ripser that compute the births and deaths
+    of homology classes of Vietoris-Rips complexes up to the desired
+    order for a given (Numpy) distance matrix (dm).
 
-	proc.wait()
-	# output = proc.stdout.read()
-	
-	output_list = output.split('\n')
-	n_points_output = int(output_list[0].split(' ')[3])
-	# assert(n_points == n_points_output)
-	matrix_min, matrix_max = output_list[1].split(' ')[2].strip('[').strip(']').split(',')
-	matrix_min = float(matrix_min)
-	matrix_max = float(matrix_max)
-	# print(matrix_min, matrix_max)
-	row = 2
-	# for ord in range(ord_max + 1):
-	ord = -1
-	result = {}
-	while output_list[row] != '':
-		if output_list[row].startswith('persistence intervals in dim '):
-			ord = int(output_list[row].split(' ')[-1][:-1])
-			row += 1
-			result_ord = []
-			result[ord] =result_ord
-			continue
+    """
+    matrix_filename = mktemp()
+    np.savetxt(matrix_filename, dm, delimiter=' ')
+    result = execute_and_parse(matrix_filename=matrix_filename,
+                               ord_max=ord_max,
+                               ripser_pathname=ripser_pathname,
+                               ripser_format=ripser_format)
+    remove(matrix_filename)
+    return result
 
-		birth, death = output_list[row].strip(' []()').split(',')
-		birth = float(birth)
-		try:
-			death = float(death)
-		except ValueError:
-			death = np.nan
 
-		result_ord.append((birth, death))
+def execute_and_parse(matrix_filename, ord_max=2,
+                      ripser_pathname=RIPSER_PATHNAME,
+                      ripser_format='distance'):
+    """Lower-level wrapper of ripser, which operates on a txt matrix saved
+    on file.
+    """
+    command = [ripser_pathname,
+               '--dim', str(ord_max),
+               '--format', ripser_format,
+               matrix_filename]
+    print("Executing: %s" % " ".join(command))
+    proc = subprocess.Popen(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    # Print output on screen and keep a copy in variable 'output':
+    output = ''
+    for line in proc.stdout:
+        print(line[:-1])
+        output += line
 
-		row += 1
+    proc.wait()
 
-	return result
+    # Parse output:
+    print("")
+    print("Parsing ripser output...")
+    output_list = output.split('\n')
+    n_points_output = int(output_list[0].split(' ')[3])
+    print("Number of points: %s" % n_points_output)
+    matrix_min, matrix_max = output_list[1].split(' ')[2].strip('[').strip(']').split(',')
+    matrix_min = float(matrix_min)
+    matrix_max = float(matrix_max)
+    row = 2
+    ord = None
+    result = {}
+    while output_list[row] != '':
+        if output_list[row].startswith('persistence intervals in dim '):
+            ord = int(output_list[row].split(' ')[-1][:-1])
+            row += 1
+            result_ord = []
+            result[ord] = result_ord
+            print("Parsing results of order: %s" % ord)
+            continue
+
+        birth, death = output_list[row].strip(' []()').split(',')
+        birth = float(birth)
+        try:
+            death = float(death)
+        except ValueError:  # this occurs when death is infinity
+            death = np.nan
+
+        result_ord.append((birth, death))
+        row += 1
+
+    return result
 
 
 if __name__ == '__main__':
-	from scipy.spatial import distance_matrix
-	n_points = 100
-	dim = 5
-	matrix_filename = '/home/stefania/ripser/examples/random_50.txt'
-	ord_max = 2
-	ripser_pathname = '/home/stefania/ripser/ripser'
-	ripser_format = 'distance'
-
-	X = np.random.uniform(size=(n_points, dim))
-	dm = distance_matrix(X, X)
-	result = ripser(dm, ord_max=ord_max)
-
+    from scipy.spatial import distance_matrix
+    n_points = 100
+    dim = 5
+    ord_max = 2
+    X = np.random.uniform(size=(n_points, dim))
+    dm = distance_matrix(X, X)
+    result = ripser(dm, ord_max=ord_max)
